97dde51a584010a307868aca0273e0cf9d98abab
dfriedenbach <danielfriedenbach@gmail.com>
2015-07-03 11:40:14 -0700

    Hash table extra credit

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,9 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._population = 0;
+  this._upperThreshold = 0.75;
+  this._lowerThreshold = 0.25;
   for (var i = 0; i < this._limit; i++) {
     this._storage.set(i, []);
   }
@@ -9,6 +12,11 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   this._storage.get(i).push([k, v]);
+  this._population += 1;
+
+  if (this._population / this._limit > this._upperThreshold) {
+    this._repopulate(this._limit * 2);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -37,8 +45,31 @@ HashTable.prototype.remove = function(k){
   });
   if (index !== undefined) {
     tupleArray.splice(index, 1);
+    this._population--;
+    if (this._limit > 8 && this._population / this._limit < this._lowerThreshold) {
+      this._repopulate(this._limit / 2);
+    }
   }
+
+
 };
+
+HashTable.prototype._repopulate = function(newLimit) {
+  this._limit = newLimit;
+  this._population = 0;
+  var oldStorage = this._storage;
+  this._storage = LimitedArray(this._limit);
+  for (var i = 0; i < this._limit; i++) {
+    this._storage.set(i, []);
+  }  
+
+  var myself = this;
+  oldStorage.each(function(bucket) {
+    bucket.forEach(function(tuple) {
+      HashTable.prototype.insert.apply(myself, tuple);
+    });
+  });
+}
 
 
 
