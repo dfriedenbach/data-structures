a8ac2ac7ece75d9b1795149030338eccd4161007
dfriedenbach <danielfriedenbach@gmail.com>
2015-07-03 11:13:30 -0700

    Implement hash table without using objects :)

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,28 +2,42 @@ var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
   for (var i = 0; i < this._limit; i++) {
-    this._storage.set(i, {});
+    this._storage.set(i, []);
   }
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.get(i)[k] = v;
+  this._storage.get(i).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var retrieved = this._storage.get(i);
-  if (!Object.keys(retrieved).length) {
-    return null;
-  } else {
-    return retrieved[k];
+  var value;
+  retrieved.forEach(function(tuple) {
+    if (tuple[0] === k) {
+      value = tuple[1];
+    }
+  });
+  if (value === undefined) {
+    value = null;
   }
+  return value;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  delete this._storage.get(i)[k];
+  var tupleArray = this._storage.get(i);
+  var index;
+  tupleArray.forEach(function(tuple, key) {
+    if (tuple[0] === k) {
+      index = key;
+    }
+  });
+  if (index !== undefined) {
+    tupleArray.splice(index, 1);
+  }
 };
 
 
